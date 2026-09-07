28ac891cc029fbe88463cc370db0b18a8cf33ee1
ucurexaquino-del <ucurexaquino@gmail.com>
2026-09-07 11:35:44 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
+  output: "export",
+
+  basePath:
+    process.env.NODE_ENV === "production"
+      ? "/pangasinan-heritage-main"
+      : "",
+
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? "/pangasinan-heritage-main/"
+      : "",
+
+  images: {
+    unoptimized: true,
+  },
+
 };
 
 export default nextConfig;
