eb5a0a4d6e26e25ce9c6b07424a99352967fc646
ucurexaquino-del <ucurexaquino@gmail.com>
2026-09-07 12:00:14 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
 
   basePath:
     process.env.NODE_ENV === "production"
-      ? "/pangasinan-heritage-main"
+      ? "/pangasinan-heritage"
       : "",
 
   assetPrefix:
